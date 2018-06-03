f726cb3e1352953a95a598b37780e3857c725c74
daperezlojo <32535705+daperezlojo@users.noreply.github.com>
2018-06-03 20:20:05 +0200

    .

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,15 @@
+
+package boletin26;
+
+/**
+ *
+ * @author Damian
+ */
+public class Boletin26 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
